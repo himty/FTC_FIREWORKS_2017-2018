c3d39771c7885fe192c8e664953aeaab6f5b893e
queenrhea <earthtravel2010@gmail.com>
2018-01-04 23:33:58 -0800

    Update HardwareTest.java

--- HardwareTest.java
+++ HardwareTest.java
@@ -16,17 +16,19 @@ public class HardwareTest
     public DcMotor  backleftMotor = null;
     public DcMotor  backrightMotor = null;
     public DcMotor  linearSlide = null;
-    public Servo    clawLeft = null; //position-based servo
-    public Servo    clawRight = null; //continuous rotation servo
+    public Servo    clawLeft = null;
+    public Servo    clawRight = null;
+
     public Servo    jewelStick = null;
-//    public DcMotor  ballHolder  = null;
+
+    public DcMotor  RollArm   = null;
+    //    public DcMotor  ballHolder  = null;
 //    public DcMotor  beaconPusher = null;
-//    public DcMotor  popper      = null;
-//    public Servo    ballDropper = null;
-//    public Servo    beaconPusher= null;
-    public CompassSensor compSensor = null;
-    public Accelerometer accelSensor = null; //this sensor is user-defined
-//    public LightSensor lightSensor = null;
+////    public DcMotor  popper      = null;
+////    public Servo    ballDropper = null;
+////    public Servo    beaconPusher= null;
+////    public CompassSensor compSensor = null;
+////    public LightSensor lightSensor = null;
 //
     /* Local OpMode members. */
     HardwareMap hwMap  = null;
@@ -47,33 +49,36 @@ public class HardwareTest
         backleftMotor  = hwMap.dcMotor.get("backleft_drive");
         backrightMotor = hwMap.dcMotor.get("backright_drive");
         frontleftMotor.setDirection(DcMotor.Direction.FORWARD);
-        frontrightMotor.setDirection(DcMotor.Direction.FORWARD);
+        frontrightMotor.setDirection(DcMotor.Direction.REVERSE);
         backleftMotor.setDirection(DcMotor.Direction.FORWARD);
         backrightMotor.setDirection(DcMotor.Direction.REVERSE);
 
-        clawLeft = hwMap.servo.get("servo_left");
-        clawRight = hwMap.servo.get("servo_right");
-        clawLeft.setPosition(0.1);
-        clawRight.setPosition(0.5);
-
-        jewelStick = hwMap.servo.get("jewel_stick");
-        jewelStick.setPosition(0);
-
-//        clawLeft.setDirection(Servo.Direction.FORWARD);
-//        clawRight.setDirection(Servo.Direction.FORWARD);
-//
-//        popper      = hwMap.dcMotor.get("popper");
+        clawLeft = hwMap.servo.get("claw_left");
+        clawRight = hwMap.servo.get("claw_right");
         linearSlide = hwMap.dcMotor.get("linear_slide");
-//        ballHolder = hwMap.dcMotor.get("ball_holder");
-//        beaconPusher = hwMap.dcMotor.get("beacon_pusher");
-//        beaconPusher.setDirection(DcMotorSimple.Direction.REVERSE);
+        jewelStick = hwMap.servo.get("jewel_stick");
+        RollArm = hwMap.dcMotor.get("roll_arm");
+
+        RollArm.setDirection(DcMotor.Direction.FORWARD);
+        linearSlide.setDirection(DcMotor.Direction.REVERSE);
+
+        clawLeft.setPosition(0.5);
+        clawRight.setPosition(0.5);
+        jewelStick.setPosition(0);
+        //RollArm.setPower(0);
+//
+////        popper      = hwMap.dcMotor.get("popper");
+
+////        ballHolder = hwMap.dcMotor.get("ball_holder");
+////        beaconPusher = hwMap.dcMotor.get("beacon_pusher");
+////        beaconPusher.setDirection(DcMotorSimple.Direction.REVERSE);
 //
 //        // Set all motors to run without encoders.
 //        // May want to use RUN_USING_ENCODERS if encoders are installed.
 //        linearSlide.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-//        ballHolder.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-//        beaconPusher.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-//        popper.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+////        ballHolder.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+////        beaconPusher.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+////        popper.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 //        leftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 //        leftMotor2.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 //        rightMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
@@ -81,9 +86,9 @@ public class HardwareTest
 //
 //
 //        linearSlide.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-//        ballHolder.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-//        beaconPusher.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-//        popper.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+////        ballHolder.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+////        beaconPusher.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+////        popper.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 //        leftMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 //        leftMotor2.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 //        rightMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
@@ -93,10 +98,10 @@ public class HardwareTest
 //        leftMotor2.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 //        rightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 //        rightMotor2.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        linearSlide.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-//        ballHolder.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-//        beaconPusher.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-//        popper.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        //linearSlide.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+////        ballHolder.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+////        beaconPusher.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+////        popper.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 //
 //
 //
@@ -106,25 +111,22 @@ public class HardwareTest
 //        rightMotor.setPower(0);
 //        rightMotor2.setPower(0);
 //        linearSlide.setPower(0);
-//        ballHolder.setPower(0);
-//        beaconPusher.setPower(0);
-//        popper.setPower(0);
+////        ballHolder.setPower(0);
+////        beaconPusher.setPower(0);
+////        popper.setPower(0);
 //
 //
-//        // Define and initialize ALL installed servos.
-//        ballDropper = hwMap.servo.get("ball_dropper");
-//        beaconPusher = hwMap.servo.get("beacon_pusher");
-//
-//        ballDropper.setPosition(0);
-//        beaconPusher.setPosition(0.5);
+////        // Define and initialize ALL installed servos.
+////        ballDropper = hwMap.servo.get("ball_dropper");
+////        beaconPusher = hwMap.servo.get("beacon_pusher");
+////
+////        ballDropper.setPosition(0);
+////        beaconPusher.setPosition(0.5);
 //
 //
 //        //Define sensors
-        compSensor = hwMap.compassSensor.get("compass_sensor");
-        compSensor.setMode(CompassSensor.CompassMode.MEASUREMENT_MODE);
-
-        accelSensor = new Accelerometer(hwMap);
-//        lightSensor = hwMap.lightSensor.get("light_sensor");
+////        compSensor = hwMap.compassSensor.get("compass_sensor");
+////        lightSensor = hwMap.lightSensor.get("light_sensor");
     }
 
     /***
