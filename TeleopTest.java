cbd75312610d105d71efb45216e9005ea66fdc4e
queenrhea <earthtravel2010@gmail.com>
2018-02-11 11:27:50 -0800

    Update TeleopTest.java

--- TeleopTest.java
+++ TeleopTest.java
@@ -24,6 +24,10 @@ public class TeleopTest extends LinearOpMode {
     final double JEWEL_STICK_MAX = 0.56;
     final double JEWEL_STICK_MIN = 0.07;
 
+    final double DRIVE_POWER_CHANGE_MAX = 0.01;
+
+    public boolean prevDPadDownState = false;
+
     //stores our instance of the Vuforia localization engine
     VuforiaLocalizerImplSubclass vuforia;
 
@@ -51,14 +55,19 @@ public class TeleopTest extends LinearOpMode {
         while (opModeIsActive()) {
             doDriveTrain();
             doClamping();
-            doLinearSlide();
-            RollOutArm();
+//            doLinearSlide();
+//            RollOutArm();
             doJewelHitter();
 
             telemetry.addData("hi", "hello");
             telemetry.addData("servoleft", robot.clawLeft.getPosition());
             telemetry.addData("servoright", robot.clawRight.getPosition());
             telemetry.addData("Jewel Stick", robot.jewelStick.getPosition());
+
+            telemetry.addData("frontLeft", robot.frontleftMotor.getPower());
+            telemetry.addData("frontRight", robot.frontrightMotor.getPower());
+            telemetry.addData("backLeft", robot.backleftMotor.getPower());
+            telemetry.addData("backRight", robot.backrightMotor.getPower());
 
             // Send telemetry message to signify robot waiting
             telemetry.update();
@@ -74,22 +83,46 @@ public class TeleopTest extends LinearOpMode {
     private void doDriveTrain() {
         //Defining which buttons on joystick corresponds to motor left and right power
         //Calculate left and right motor drivetrain power
-        targetLeftPower = (gamepad1.right_stick_y - gamepad1.right_stick_x *2);
-        targetRightPower = (gamepad1.right_stick_y + gamepad1.right_stick_x * 2);
+        double r = Math.hypot(gamepad1.left_stick_x, gamepad1.left_stick_y);
+        double robotAngle = Math.atan2(gamepad1.left_stick_y, gamepad1.left_stick_x) - Math.PI / 4;
+        //Turning is configured on separate joystick
+        double rightX = gamepad1.right_stick_x;
+        final double v1 = r * Math.cos(robotAngle) + rightX;
+        final double v2 = r * Math.sin(robotAngle) - rightX;
+        final double v3 = r * Math.sin(robotAngle) + rightX;
+        final double v4 = r * Math.cos(robotAngle) - rightX;
 
-        //If bumper on joystick is not pressed, decrease motor power
-        if (!gamepad1.right_bumper){
-            targetLeftPower /= 5;
-            targetRightPower /= 5;
-        }
+//        targetLeftPower = (gamepad1.right_stick_y - gamepad1.right_stick_x * 2);
+//        targetRightPower = (gamepad1.right_stick_y + gamepad1.right_stick_x * 2);
+//
+//        //If bumper on joystick is not pressed, decrease motor power
+//        if (!gamepad1.right_bumper) {
+//            targetLeftPower /= 5;
+//            targetRightPower /= 5;
+//        }
+//
+//        if (Math.abs(targetLeftPower - robot.frontleftMotor.getPower()) > DRIVE_POWER_CHANGE_MAX) {
+//            //and if the robot is trying to accelerate too much
+//            if (targetLeftPower > robot.frontleftMotor.getPower()) {
+//                targetLeftPower = robot.frontleftMotor.getPower() + DRIVE_POWER_CHANGE_MAX;
+//            } else {
+//                targetLeftPower = robot.frontleftMotor.getPower() - DRIVE_POWER_CHANGE_MAX;
+//            }
+//        }
+//        if (Math.abs(targetRightPower - robot.frontrightMotor.getPower()) > DRIVE_POWER_CHANGE_MAX) {
+//            if (targetRightPower > robot.frontrightMotor.getPower()) {
+//                targetRightPower = robot.frontrightMotor.getPower() + DRIVE_POWER_CHANGE_MAX;
+//            } else {
+//                targetRightPower = robot.frontrightMotor.getPower() - DRIVE_POWER_CHANGE_MAX;
+//            }
+//        }
 
         //Set powers
-        robot.frontleftMotor.setPower(targetLeftPower);
-        robot.backleftMotor.setPower(targetLeftPower);
-        robot.frontrightMotor.setPower(targetRightPower);
-        robot.backrightMotor.setPower(targetRightPower);
+       robot.frontleftMotor.setPower(v1);
+       robot.frontrightMotor.setPower(v2);
+       robot.backleftMotor.setPower(v3);
+       robot.backrightMotor.setPower(v4);
     }
-
 
     private void doClamping() {
         if (gamepad1.dpad_up) {
@@ -97,44 +130,66 @@ public class TeleopTest extends LinearOpMode {
             //0.7 and 0.2 is range of values
             robot.clawLeft.setPosition(0.7);
             robot.clawRight.setPosition(0.2);
-        } else if (gamepad1.dpad_down) {
+        }
+        if (gamepad1.dpad_down) {
             //make claws release the object
             robot.clawLeft.setPosition(0.2);
             robot.clawRight.setPosition(0.7);
+
+            prevDPadDownState = true;
         }
         else {
-            //Make claws hang loose if there is nothing to do
-            robot.clawLeft.setPosition(0.5);
-            robot.clawRight.setPosition(0.5);
+            if (prevDPadDownState == true) {
+                robot.clawLeft.setPosition(0.5);
+                robot.clawRight.setPosition(0.5);
+            }
+            prevDPadDownState = false;
         }
     }
 
     /**
      * Linear slide moves up and down based on pressing the right joystick
      */
-    private void doLinearSlide() {
-        robot.linearSlide.setPower(gamepad2.right_stick_y * -1);
-    }
+//    private void doLinearSlide() {
+//        robot.linearSlide.setPower(gamepad2.right_stick_y * -1);
+//    }
 
     /**
      * Horizontal lift comes out by pressing left joystick
      */
-    private void RollOutArm() {
-        robot.RollArm.setPower(gamepad2.left_stick_y);
-    }
+//    private void RollOutArm() {
+//        robot.rollArm.setPower(gamepad2.left_stick_y);
+//    }
 
     /**
      * The Jewel Stick hits one of the balls based on the team color
      * This method lowers and raises the stick
      * Jewel Stick travels in Increments of 0.15 (speed)
      */
+//    private void doJewelHitter() {
+//        double currPosition = robot.jewelStick.getPosition();
+//        if (gamepad2.dpad_down) {
+//            robot.jewelStick.setPosition(Math.min(currPosition + 0.15, JEWEL_STICK_MAX));
+//        }
+//        if (gamepad2.dpad_up) {
+//            robot.jewelStick.setPosition(Math.max(currPosition - 0.15, JEWEL_STICK_MIN));
+//        }
+//    }
+
     private void doJewelHitter() {
-        double currPosition = robot.jewelStick.getPosition();
+        //double currPosition = robot.jewelStick.getPosition();
         if (gamepad2.dpad_down) {
-            robot.jewelStick.setPosition(Math.min(currPosition + 0.15, JEWEL_STICK_MAX));
+            //make jewelstick go down
+            robot.jewelStick.setPosition(JEWEL_STICK_MAX);
+
+        } else if (gamepad2.dpad_up) {
+            //make jewelstick go up
+            robot.jewelStick.setPosition(JEWEL_STICK_MIN);
         }
-        if (gamepad2.dpad_up) {
-            robot.jewelStick.setPosition(Math.max(currPosition - 0.15, JEWEL_STICK_MIN));
+        else {
+            //Make jewel stick hang loose if there is nothing to do
+            robot.jewelStick.setPosition(0.01);
         }
+
     }
 }
